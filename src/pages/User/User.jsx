e4c21fe5ae939a6acaf4313145063e4bdc165dfe
gabriel-codes <gabrielss@outlook.com>
2020-11-27 20:04:01 -0300

    Adding a simple avatar on User Page

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { MdPerson } from 'react-icons/md';
 
 const User = () => {
-  return <div>USer</div>;
+  return (
+    <div>
+      <MdPerson size={100} color="#999" />
+    </div>
+  );
 };
 
 export default User;
